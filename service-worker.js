7887fa02f4f4aa83ac191e88390ee9755d1974fe
ClarkeFL <fabioliecl@gmail.com>
2022-12-01 17:16:09 +1000

    updating service worker

--- service-worker.js
+++ service-worker.js
@@ -4,3 +4,18 @@ workbox.routing.registerRoute(
     ({ request }) => request.destination === 'image',
     new workbox.strategies.CacheFirst()
 );
+
+const CACHE = "offline-pages";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
+});
+
+workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+        cacheName: CACHE
+    })
+);
